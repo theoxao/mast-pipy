3a70f33f24387a983ad9d468714042651be47e2c
theo <theoxao@163.com>
2021-10-26 21:51:57 +0800

    gpio

--- mast/pi.py
+++ mast/pi.py
@@ -1,5 +1,34 @@
 import RPi.GPIO as gpio
 
 
+g.setmode(g.BCM)
+
+g.setup(2, g.OUT, initial= g.LOW)
+g.setup(3, g.OUT, initial= g.LOW)
+g.setup(4, g.OUT, initial= g.LOW)
+
+
+def move(value):
+    g.output(2, value)
+    g.output(4,True)
+    g.output(4,False)
+
+
+def commit():
+    g.output(3, True)
+    g.output(3,False)
+
 def update_state(position, value):
-    pass
+    high = position*2 + value
+    for i in range(high-1):
+        move(False)
+    move(True)
+    for i in range(32-high):
+        move(False)
+    commit()
+    
+    for i in range(32):
+        move(False)
+    commit()
+        
+    
