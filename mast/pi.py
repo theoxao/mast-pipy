50ba1ea0fb9536adc1714317d940ed1a87aa3a96
theo <theoxao@163.com>
2021-11-14 23:12:10 +0800

    set oe

--- mast/pi.py
+++ mast/pi.py
@@ -1,3 +1,5 @@
+import time
+
 import RPi.GPIO as g
 from time import sleep
 
@@ -6,6 +8,7 @@ g.setmode(g.BCM)
 g.setup(2, g.OUT, initial= g.LOW)
 g.setup(3, g.OUT, initial= g.LOW)
 g.setup(4, g.OUT, initial= g.LOW)
+g.setup(17,g.OUT, initial = g.LOW)
 
 
 def move(value):
@@ -21,18 +24,22 @@ def commit():
 
 def update_state(position, value):
     high = position*2 + int(value)
+    g.output(17, True)
     for i in range(high-1):
         move(False)
-        sleep(0.05)
+        commit()
     move(True)
+    commit()
     for i in range(32-high):
         move(False)
-        sleep(0.05)
-    commit()
-    # for i in range(32):
-    #     move(False)
-    #     sleep(0.05)
-    # commit()
-    sleep(0.1)
+        commit()
+    time.sleep(1)
+    g.output(17, False)
+    time.sleep(1)
+    g.output(17, True)
+    for i in range(32):
+        move(False)
+        commit()
+    g.output(17, False)
         
     
