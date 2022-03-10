25ce2b119b7be11ccf52a6dc438accca6c8b5867
theo <theoxao@163.com>
2022-03-10 19:20:03 +0800

    error handler

--- mast/pi.py
+++ mast/pi.py
@@ -7,8 +7,8 @@ pi_host = "http://172.29.236.153:8000/api"
 
 def update_state(position, value, detect):
     return requests.post(pi_host + "/update_state",
-                         data=json.dumps({"position": position, "value": value, "detect": detect}))
+                         data=json.dumps({"position": position, "value": value, "detect": detect})).json()["data"]
 
 
 def query_state(position):
-    return requests.post(pi_host + "/query_state", data=json.dumps({"position": position}))
+    return requests.post(pi_host + "/query_state", data=json.dumps({"position": position})).json()["data"]
