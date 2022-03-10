41788955e14697da4b7176f9774f600ba89cbd2e
theo <theoxao@163.com>
2022-03-10 19:13:29 +0800

    error handler

--- mast/api.py
+++ mast/api.py
@@ -35,7 +35,7 @@ def ok(result):
 
 @bp.app_errorhandler(HTTPException)
 def handle_invalid_usage(error):
-    return jsonify(code=error.code, message=error.__str__, data=None)
+    return jsonify(code=500, message=error.__str__, data=None)
 
 
 @bp.route("/aligenie/task", methods=['POST'])
