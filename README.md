0564e80e3033c646b43cd354611866592bc13ed8
theo <theoxao@163.com>
2022-04-11 20:34:43 +0800

    error handler

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 1.服务部署到ecs  
 2.zt网络调用树莓派   
-3.树莓派心跳机制  
+3.树莓派心跳机制
+
+流程： 1收到请求后，调用query_state查询当前设备状态 1.1若设备状态和请求状态一直，更新数据库value状态 1.2不一致调用update_state，再次query_state判断是否成功 1.2.1成功返回
+1.2.2不成功，切换direction后再次调用update_state,
--- mast/api.py
+++ mast/api.py
@@ -38,6 +38,11 @@ def handle_invalid_usage(error):
     return jsonify(code=500, message="internal error", data=None)
 
 
+@bp.route("/aligenie/task", methods=['GET'])
+def auth_aligenie():
+    return "Jfc4Z4Ur15JwUBuvUQD5wg7Nu8+l+HscqYlfofbyJdYrwmXUPrPUrLoC7K/NBG4h"
+
+
 @bp.route("/aligenie/task", methods=['POST'])
 def aligenie_task():
     query = request.get_json()
@@ -48,18 +53,20 @@ def aligenie_task():
     logging.log(logging.DEBUG, v0 + '----->' + v1)
     pos = -1
     if v1 == '客厅灯光':
-        pos = 1
-    if v1 == '餐厅灯光':
-        pos = 2
-    if v1 == '次卧灯光':
-        pos = 3
-    if v1 == '主卧背光':
         pos = 4
-    if v1 == '书房灯光':
+    if v1 == '餐厅灯光':
         pos = 5
-    if v1 == '主卧灯带':
+    if v1 == '次卧灯光':
         pos = 6
+    if v1 == '主卧背光':
+        pos = 3
+    if v1 == '书房灯光':
+        pos = 0
+    if v1 == '主卧灯带':
+        pos = 2
     if v1 == '主卧灯光':
+        pos = 1
+    if v1 == '厨房灯光':
         pos = 7
     conn = get_db()
     result = query_db('select * from device where position = ?', (pos,))[0]
